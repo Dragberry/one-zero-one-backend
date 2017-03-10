cf5c785ff063254d587f103f4af0e01c0a71cd88
Maksim Drahun <maksimdragun@live.com>
2017-03-11 02:27:56 +0300

    time utils to common

---
+++ ozo-common/src/main/java/org/dragberry/ozo/common/level/TimeUtils.java
@@ -0,0 +1,22 @@
+package org.dragberry.ozo.common.level;
+
+public final class TimeUtils {
+	
+	private static final int SECONDS_PER_MINUTE = 60;
+	private static final String EMPTY = "";
+	private static final String ZERO = "0";
+	private static final String COLON = ":";
+	
+	private TimeUtils() {}
+	
+	public static String timeToString(int timeInSeconds) {
+		int minutes = timeInSeconds / SECONDS_PER_MINUTE;
+		int seconds = timeInSeconds % SECONDS_PER_MINUTE;
+		return prefixZero(minutes) + COLON + prefixZero(seconds);
+	}
+	
+	private static String prefixZero(int time) {
+		return (time < 10 ? ZERO : EMPTY) + time;
+	}
+
+}
--- ozo-common/src/main/java/org/dragberry/ozo/common/levelresult/LevelResultName.java
+++ ozo-common/src/main/java/org/dragberry/ozo/common/levelresult/LevelResultName.java
@@ -1,8 +1,20 @@
 package org.dragberry.ozo.common.levelresult;
 
+import org.dragberry.ozo.common.level.TimeUtils;
+
 public enum LevelResultName {
 	
-	TIME, STEPS, LOST_UNITS;
+	TIME(new TimeBuilder()), STEPS(new StringBuilder()), LOST_UNITS(new StringBuilder());
+	
+	private StringBuilder stringBuilder;
+	
+	private LevelResultName(StringBuilder builder) {
+		this.stringBuilder = builder;
+	}
+	
+	public String key() {
+		return "ozo.result." + name();
+	}
 	
 	public String personal() {
 		return name() + ".result.personal";
@@ -16,4 +28,22 @@ public enum LevelResultName {
 		return name() + ".result.owner";
 	}
 
+	public String toString(Integer value) {
+		return stringBuilder.build(value);
+	}
+	
+	private static class StringBuilder {
+		
+		String build(Integer value) {
+			return value.toString();
+		}
+	}
+	
+	private static class TimeBuilder extends StringBuilder {
+		
+		@Override
+		String build(Integer value) {
+			return TimeUtils.timeToString(value);
+		}
+	}
 }
